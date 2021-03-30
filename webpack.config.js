5e08247c988b37b29c68d2fd6af0a4cb13ff6004
Vitaliy <i@kassad.ru>
2021-03-30 20:09:09 +0500

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = env => {
-  const mode = env && env.production ? "production" : "development";
+module.exports = (env, argv) => {
+  const mode = argv && argv.mode;
   console.log("Mode: ", mode);
 
   return {
